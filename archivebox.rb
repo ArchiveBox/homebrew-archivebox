844264089d96b9bdf1cc55e7e1bc7ab738de58c9
Nick Sweeting <git@sweeting.me>
2024-02-18 06:41:18 -0800

    Update archivebox.rb

--- archivebox.rb
+++ archivebox.rb
@@ -240,7 +240,9 @@ class Archivebox < Formula
     system "python3.11", "-m", "pip", "--python=#{prefix}/libexec/bin/python", "install", "--upgrade", "--ignore-installed", "archivebox[sonic,ldap]", "yt-dlp", "playwright"
     system "mkdir", "-p", "#{HOMEBREW_PREFIX}/var/archivebox/data"
     cd "#{HOMEBREW_PREFIX}/var/archivebox/data" do
-      system "#{bin}/archivebox", "init", "--setup"
+      system "#{bin}/archivebox", "init"
+      system "#{bin}/archivebox", "manage", "createsuperuser", "--no-input", "--username=admin", "--email=homebrew-admin@example.local"
+      system "#{bin}/archivebox", "setup"
     end
     # system "#{bin}/../libexec/bin/playwright", "install", "chromium"
   end
